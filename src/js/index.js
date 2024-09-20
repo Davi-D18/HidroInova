160d48b81bc95f71e91fb3165247977dc79390c0
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2024-09-20 15:41:23 -0300

    Ajustes na logo, página do projeto e cores nos cabeçalhos

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,22 @@
 // Função para redirecionar para a URL especificada no atributo data-url do card clicado
 function handleCardClick(event) {
   const card = event.currentTarget;
+  const screenWidth = window.innerWidth;
+
+  // Redirecionar automaticamente se a tela for maior que 768px
+  if (screenWidth > 768) {
+    const url = card.getAttribute("data-url");
+    if (url) {
+      window.location.href = url;
+    }
+  }
+}
+
+// Função para redirecionar ao clicar no botão em dispositivos móveis
+function handleButtonClick(event) {
+  event.stopPropagation(); // Impede que o clique no botão acione o evento do card
+  const button = event.currentTarget;
+  const card = button.closest("section");
   const url = card.getAttribute("data-url");
 
   if (url) {
@@ -8,12 +24,17 @@ function handleCardClick(event) {
   }
 }
 
-// Adiciona o event listener a todos os cards na página
+// Adiciona os event listeners aos cards e botões
 function setupCardRedirects() {
   const cards = document.querySelectorAll("section");
+  const buttons = document.querySelectorAll(".pages-link");
 
   cards.forEach((card) => {
     card.addEventListener("click", handleCardClick);
+  });
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", handleButtonClick);
   });
 }
 
